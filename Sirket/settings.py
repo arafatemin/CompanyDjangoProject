98456b59eb7d37b6fe8533deefe5d6c5331a95ec
AERBATI-61 <jasurboy52@gmail.com>
2022-05-10 04:09:40 +0300

    Merge

--- Sirket/settings.py
+++ Sirket/settings.py
@@ -128,8 +128,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'meida')
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = BASE_DIR/'staticfiles'
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
