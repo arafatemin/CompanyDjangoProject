3e2c7e835b05072347b1b564d40a0c3be201d759
lenovo <jasurboy52@gmail.com>
2022-10-14 11:11:27 +0600

    Merge

--- Sirket/settings.py
+++ Sirket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%ww$@$w$o#gbgv!---k8vgorh75%jf37mdv=hrgzzp5o1cbw-i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
